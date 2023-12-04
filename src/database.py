59eeb4ea1082db44539ceb82fd3fc0dd42b20a17
Ivan_Shebankov <shebankov.ia@phystech.edu>
2023-12-04 22:48:47 +0300

    new features

--- src/database.py
+++ src/database.py
@@ -14,91 +14,86 @@ class dbworker:
                             "sex TEXT,"
                             "age INTEGER,"
                             "social_link TEXT)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS cities(city TEXT PRIMARY KEY)")
 
+    def add_city(self, city):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO `cities` (`city`) VALUES(?)", (city,))
+
+    def city_exists(self, city):
+        with self.connection:
+            result = self.cursor.execute('SELECT * FROM `cities` WHERE `city` = ?', (city,)).fetchall()
+            return bool(len(result))
 
     def user_exists(self, user_id):
-        '''Проверка есть ли юзер в бд'''
         with self.connection:
             result = self.cursor.execute('SELECT * FROM `users` WHERE `telegram_id` = ?', (user_id,)).fetchall()
             return bool(len(result))
 
     def add_user(self, telegram_username, telegram_id, full_name):
-        '''Добавляем нового юзера'''
         with self.connection:
             return self.cursor.execute(
                 "INSERT INTO `users` (`telegram_username`, `telegram_id`,`full_name`) VALUES(?,?,?)",
                 (telegram_username, telegram_id, full_name))
 
     def create_profile(self, telegram_id, telegram_username, name, description, city, photo, sex, age, social_link):
-        '''Создаём анкету'''
         with self.connection:
             return self.cursor.execute(
                 "INSERT INTO `profile_list` (`telegram_id`,`telegram_username`,`name`,`description`,`city`,`photo`,`sex`,`age`,`social_link`) VALUES(?,?,?,?,?,?,?,?,?)",
                 (telegram_id, telegram_username, name, description, city, photo, sex, age, social_link))
 
     def profile_exists(self, user_id):
-        '''Проверка есть ли анкета в бд'''
         with self.connection:
             result = self.cursor.execute('SELECT * FROM `profile_list` WHERE `telegram_id` = ?', (user_id,)).fetchall()
             return bool(len(result))
 
     def delete_profile(self, user_id):
-        '''Удаление анкеты'''
         with self.connection:
             return self.cursor.execute("DELETE FROM `profile_list` WHERE `telegram_id` = ?", (user_id,))
 
     def all_profile(self, user_id):
-        '''поиск по анкетам'''
         with self.connection:
             return self.cursor.execute("SELECT * FROM `profile_list` WHERE `telegram_id` = ?", (user_id,)).fetchall()
 
     def edit_description(self, description, user_id):
-        '''изменение описания'''
         with self.connection:
             return self.cursor.execute('UPDATE `profile_list` SET `description` = ? WHERE `telegram_id` = ?',
                                        (description, user_id))
 
     def edit_age(self, age, user_id):
-        '''изменение возвраста'''
         with self.connection:
             return self.cursor.execute('UPDATE `profile_list` SET `age` = ? WHERE `telegram_id` = ?', (age, user_id))
 
-    def search_profile(self, city, age, sex):
-        '''поиск хаты'''
+    def search_profile(self, city, sex):
         try:
-            if str(sex) == 'мужчина':
-                sex_search = 'женщина'
+            if str(sex) == 'М':
+                sex_search = 'Ж'
             else:
-                sex_search = 'мужчина'
+                sex_search = 'М'
             with self.connection:
                 return self.cursor.execute(
-                    "SELECT `telegram_id` FROM `profile_list` WHERE `city` = ? AND `sex` = ? ORDER BY `age` DESC",
+                    "SELECT `telegram_id` FROM `profile_list` WHERE `city` = ? AND `sex` = ?",
                     (city, sex_search)).fetchall()
         except Exception as e:
             print(e)
 
     def get_info(self, user_id):
-        '''получение ифнормации по профилю'''
         with self.connection:
             return self.cursor.execute("SELECT * FROM `profile_list` WHERE `telegram_id` = ?", (user_id,)).fetchone()
 
     def search_profile_status(self, user_id):
-        '''возвращение статуса'''
         with self.connection:
             return self.cursor.execute("SELECT `search_id` FROM `users` WHERE `telegram_id` = ?", (user_id,)).fetchone()
 
     def edit_profile_status(self, user_id, num):
-        '''изменение статуса'''
         with self.connection:
             return self.cursor.execute('UPDATE `users` SET `search_id` = ? WHERE `telegram_id` = ?',
                                        (str(num + 1), user_id))
 
     def edit_zero_profile_status(self, user_id):
-        '''изменение статуса на 0 когда анкеты заканчиваются'''
         with self.connection:
             return self.cursor.execute('UPDATE `users` SET `search_id` = 0 WHERE `telegram_id` = ?', (user_id,))
 
     def get_info_user(self, user_id):
-        '''получение информации по юзеру'''
         with self.connection:
             return self.cursor.execute("SELECT * FROM `users` WHERE `telegram_id` = ?", (user_id,)).fetchone()
---
+++ start.py
@@ -0,0 +1,60 @@
+import asyncio
+import logging
+
+from aiogram.types import FSInputFile
+from aiogram.fsm.state import StatesGroup, State
+
+from src.handlers import *
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='src/bot.log')
+
+
+"""import pandas as pd
+подгружаю все города РФ из википедии и закидываю в бд
+url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8'
+df = pd.read_html(url)[0]
+for city in df['Город']:
+    if not db.city_exists(city):
+        db.add_city(city)"""
+
+
+class CreateProfile(StatesGroup):
+    name = State()
+    description = State()
+    city = State()
+    photo = State()
+    sex = State()
+    age = State()
+    social_link = State()
+
+
+class SearchPair(StatesGroup):
+    SearchStart = State()
+    InActiveSearch = State()
+
+
+async def show_profile(from_id, user_id):
+    if db.profile_exists(user_id):
+        user_data = db.get_info(user_id)
+        info = (f"Имя: {user_data[2]}\n"
+                f"Пол: {user_data[6]}\n"
+                f"Возраст: {user_data[7]}\n"
+                f"Город: {user_data[4]}\n"
+                f"Описание: {user_data[3]}\n"
+                f"tg: @{user_data[1]}")
+        if user_data[-1] != "Скип":
+            info += f"\nСсылочка: {user_data[-1]}"
+        photo = FSInputFile(f"src/photos/{user_id}.jpg")
+        await bot.send_photo(from_id, photo, caption=info)
+    else:
+        await bot.send_message(from_id, "Ты как сюда попал, у тебя профиля нет")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
