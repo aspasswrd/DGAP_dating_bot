85231b04487f1254d753a8d7ba340ac2a2b3b58d
aspasswrd <iiivan.11@yandex.ru>
2025-03-02 23:12:02 +0300

    better async

--- user_generator.py
+++ user_generator.py
@@ -2,15 +2,8 @@ import random
 import string
 import asyncio
 import aiohttp
-from PIL import Image
 
 from src.config import get_db_connection, upload_image
-
-
-# Генерация случайного имени пользователя
-def generate_random_username():
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-
 
 # Генерация случайного возраста
 def generate_random_age():
@@ -26,7 +19,7 @@ def generate_random_location():
 # Генерация случайных предпочтений
 def generate_random_preferences():
     min_age = 14
-    max_age = random.randint(min_age, 30)
+    max_age = random.randint(20, 30)
     search_radius = random.randint(1, 50)
     return min_age, max_age, search_radius
 
@@ -41,11 +34,12 @@ async def get_random_user():
                 photo_url = user['picture']['large']
                 gender = user['gender']
                 is_male = False
+                username = user['login']['username']
                 if gender == "male":
                     is_male = True
-                return name, photo_url, is_male
+                return name, photo_url, is_male, username
             else:
-                return None, None
+                return None, None, None, None
 
 
 # Асинхронное подключение и вставка пользователя в базу данных
@@ -72,31 +66,39 @@ async def insert_photo(conn, user_id, photo):
     ''', user_id, photo)
 
 
+async def generate_and_insert_user():
+    conn = await get_db_connection()
+    user_id = random.randint(1000000000, 9999999999)
+    name, photo_url, is_male, username = await get_random_user()
+    age = generate_random_age()
+    location = generate_random_location()
+    print(name, username)
+
+    await insert_user(conn, user_id, username, name, is_male, age, location)
+
+    min_age, max_age, search_radius = generate_random_preferences()
+    await insert_preferences(conn, user_id, min_age, max_age, search_radius)
+
+    await insert_photo(conn, user_id, photo_url)
+
+    await conn.close()
+
+
+
+
 # Генерация и вставка случайных пользователей
 async def generate_and_insert_users(n):
-    conn = await get_db_connection()
+    tasks = []
+    for i in range(n):
+        tasks.append(generate_and_insert_user())
 
-    for _ in range(n):
-        user_id = random.randint(1000000000, 9999999999)
-        username = generate_random_username()
-        name, photo_url, is_male = await get_random_user()
-        age = generate_random_age()
-        location = generate_random_location()
-
-        await insert_user(conn, user_id, username, name, is_male, age, location)
-
-        min_age, max_age, search_radius = generate_random_preferences()
-        await insert_preferences(conn, user_id, min_age, max_age, search_radius)
-
-        await insert_photo(conn, user_id, photo_url)
-
-    # Закрытие соединения с базой данных
-    await conn.close()
+    await asyncio.gather(*tasks)
 
 
 # Запуск генерации пользователей
 async def main():
-    await generate_and_insert_users(500)
+    for i in range(20):
+        await generate_and_insert_users(5)
 
 
 # Запуск асинхронного кода
